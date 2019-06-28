5b121bb3d3e0575ee0db5f686b799cc88a1bdd2a
rivendell <webwyler@gmail.com>
2019-06-27 23:44:39 -0500

    make an array with 6 choices, determine user click, random letter choice, alert user and computer guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,21 @@
-document.onkeyup = function() {
-    alert("Hey, you pressed a key!");
-};
+// document.onkeyup = function() {
+//     alert("Hey, you pressed a key!");
+// }
+
+// create a test array with six letters
+var choices = ["q", "w", "e", "r", "t", "y"];
+
+// Run this function whenever the user presses a key.
+document.onkeyup = function(event) {
+
+    // determine what key the user clicks
+    var keychoice = event.key;
+
+    // Randomly choose letter from the choices array. This is the Computer's guess.
+    var computerGuess = choices[Math.floor((Math.random() * choices.length))];
+
+    alert("You clicked: " + keychoice);
+    alert("The Computer was thinking of: " + computerGuess);
+
+
+}
